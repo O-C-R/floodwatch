5a6a825b9ae100e794c77fff25eb9bd5d50d8ed5
Eric Buth <ericdotbuth@gmail.com>
2016-09-20 16:13:00 -0400

    user

---
+++ api/data/user_test.go
@@ -0,0 +1,24 @@
+package data
+
+import (
+	"testing"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+const (
+	testPassword            = "password"
+	testPasslibPasswordHash = "$6$rounds=615293$qIFB88g9CYLY42Se$D3rSnpMtHvfVQlUD7q6CqdIC5EKqLqZL8UCJPC5oNQZKhJCS2j3jBWvkw2rFYPWErt2CpZYv65M..yELyZ6OV0"
+)
+
+func TestPasslibPassword(t *testing.T) {
+	user := &User{Password: []byte(testPasslibPasswordHash)}
+
+	if err := user.CheckPassword(testPassword); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(testPassword)); err != nil {
+		t.Error(err)
+	}
+}
