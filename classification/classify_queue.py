398271822975bd2dd3d4a2db827b8c27d3ee1fb3
Eric Buth <ericdotbuth@gmail.com>
2016-11-01 16:03:05 -0400

    classify queue

---
+++ classification/classify_queue.py
@@ -0,0 +1,76 @@
+from AdClassifier import AdClassifier
+from json import JSONEncoder
+import boto3
+import numpy
+import os
+import tempfile
+import traceback
+
+class NumpyEncoder(JSONEncoder):
+	def default(self, obj):
+		if isinstance(obj, numpy.integer):
+			return int(obj)
+		if isinstance(obj, numpy.floating):
+			return float(obj)
+		if isinstance(obj, numpy.ndarray):
+			return obj.tolist()
+		return super(self, obj)
+
+jsonEncoder = NumpyEncoder()
+
+sqs = boto3.client('sqs')
+s3 = boto3.client('s3')
+classifier = AdClassifier(useGPU=True)
+
+while True:
+	receiveMessageResponse = sqs.receive_message(
+		QueueUrl=os.environ['INPUT_QUEUE_URL'],
+		MessageAttributeNames=['id','bucket','key'],
+		MaxNumberOfMessages=1,	
+		VisibilityTimeout=60,
+		WaitTimeSeconds=20
+	)
+
+	if 'Messages' not in receiveMessageResponse:
+		continue
+
+	for message in receiveMessageResponse['Messages']:
+		try:
+			getObjectResponse = s3.get_object(
+				Bucket=message['MessageAttributes']['bucket']['StringValue'],
+				Key=message['MessageAttributes']['key']['StringValue'],
+			)
+	
+			objectFile=tempfile.NamedTemporaryFile(delete=False)
+			for chunk in iter(lambda: getObjectResponse['Body'].read(1024), b''):
+				objectFile.write(chunk)
+			objectFile.close()
+	
+			classifyResult=classifier.classify(imgPath=objectFile.name)
+
+			sqs.send_message(
+				QueueUrl=os.environ['OUTPUT_QUEUE_URL'],
+				MessageBody=jsonEncoder.encode(classifyResult),
+				MessageAttributes={
+					'id': {
+						'StringValue': message['MessageAttributes']['id']['StringValue'],
+						'DataType': 'String'
+					}
+				}
+			)
+		except:
+			print(traceback.format_exc())
+		
+		try:
+			sqs.delete_message(
+				QueueUrl=os.environ['INPUT_QUEUE_URL'],
+				ReceiptHandle=message['ReceiptHandle']
+			)
+		except:
+			print(traceback.format_exc())
+
+		try:
+			if os.path.isfile(objectFile.name):
+				os.remove(objectFile.name)
+		except:
+			print(traceback.format_exc())
