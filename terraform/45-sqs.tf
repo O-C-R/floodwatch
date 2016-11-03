6e1daeea41d88510a42a85a9e74cf4bf23837e4a
Eric Buth <ericdotbuth@gmail.com>
2016-11-03 13:57:43 -0400

    sqs

---
+++ terraform/45-sqs.tf
@@ -0,0 +1,11 @@
+resource "aws_sqs_queue" "classifier-input" {
+  name = "classifier-input"
+  max_message_size = 1024
+  message_retention_seconds = 1209600
+}
+
+resource "aws_sqs_queue" "classifier-output" {
+  name = "classifier-output"
+  max_message_size = 1024
+  message_retention_seconds = 1209600
+}
