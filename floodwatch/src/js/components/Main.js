dcbf3297d7e55c6f3ec4ca923746ff37a44525e2
Chris Anderson <chanderson0@gmail.com>
2016-12-22 12:06:51 -0500

    notify user to install extension if not installed

--- floodwatch/src/js/components/Main.js
+++ floodwatch/src/js/components/Main.js
@@ -1,6 +1,6 @@
 // @flow
 import React, {Component} from 'react';
-import {Grid, Nav, Navbar, NavItem, Row, Col} from 'react-bootstrap';
+import {Grid, Nav, Navbar, NavItem, Row, Col, Button, Alert} from 'react-bootstrap';
 import {LinkContainer} from 'react-router-bootstrap';
 
 import '../../css/app.css';
@@ -10,10 +10,14 @@ import {FWApiClient} from '../api/api';
 
 import {Navigation} from './Navigation';
 
+const chrome = window.chrome;
+
 type MainState = {
   user: ?Object,
   message: ?string,
-  messageClearTimeout: ?number
+  messageClearTimeout: ?number,
+  extensionInstalled: boolean,
+  extensionInstallMsgDismissed: boolean
 };
 
 export class Main extends Component {
@@ -21,23 +25,37 @@ export class Main extends Component {
 
   constructor() {
     super();
+
     this.state = {
       user: null,
       message: null,
-      messageClearTimeout: null
+      messageClearTimeout: null,
+      extensionInstalled: false,
+      extensionInstallMsgDismissed: false
     };
 
     this.loadUserFromServer();
+    this.detectExtension();
   }
 
-  loadUserFromServer(): Promise<void> {
-    return FWApiClient.get().getCurrentPerson()
-      .then((user) => {
-        this.setState({ user: user });
-      })
-      .catch((error) => {
-        this.setState({ user: null });
-      })
+  async loadUserFromServer(): Promise<void> {
+    try {
+      const user = await FWApiClient.get().getCurrentPerson()
+      this.setState({ user: user });
+    } catch (e) {
+      this.setState({ user: null });
+    }
+  }
+
+  detectExtension(): void {
+    const hasExtension = () => {
+      if (document.body.getAttribute('data-fw-frame-id') != null) {
+        setTimeout(() => this.setState({ extensionInstalled: true }), 1);
+      } else {
+        setTimeout(hasExtension, 100);
+      }
+    }
+    hasExtension();
   }
 
   showMessage(message: string, timeout?: number) {
@@ -57,6 +75,18 @@ export class Main extends Component {
     await FWApiClient.get().logout();
     this.setState({ user: null });
     history.push('/');
+  }
+
+  installClick() {
+    chrome.webstore.install(
+      "https://chrome.google.com/webstore/detail/oiilbnnfccienlfahiecfglojnkhpgaf",
+      () => { this.setState({ extensionInstalled: true }) },
+      (err) => { console.error(err); }
+    );
+  }
+
+  dismissInstallClick() {
+    this.setState({ extensionInstallMsgDismissed: true });
   }
 
   render() {
@@ -79,8 +109,13 @@ export class Main extends Component {
 
     return (
       <Grid fluid style={{position:'relative'}}>
-        <Row>
-          {this.state.message && <div className="alert alert-info">{this.state.message}</div>}
+        <Row id="row-top">
+          { this.state.message && <Alert bsStyle="info">{this.state.message}</Alert> }
+          { this.state.user && !this.state.message && !this.state.extensionInstalled && !this.state.extensionInstallMsgDismissed &&
+            <Alert bsStyle="info" className="text-center" onDismiss={this.dismissInstallClick.bind(this)}>
+              <span style={{ paddingRight: '10px' }}>Install the extension to get started</span>
+              <Button bsStyle="primary" bsSize="small" onClick={this.installClick.bind(this)}>Add to Chrome</Button>
+          </Alert> }
           <Navigation navs={navs} />
         </Row>
         <Row>
